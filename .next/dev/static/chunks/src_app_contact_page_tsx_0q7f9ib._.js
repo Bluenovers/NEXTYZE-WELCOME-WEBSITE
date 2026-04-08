0aee2e882fded05b49e486b60a15e8f108d70b9b
Mohamed Adnan Shajahan <137816704+Bluenovers@users.noreply.github.com>
2026-04-08 21:42:06 +0400

    Clean workflows and build artifacts

--- .next/dev/static/chunks/src_app_contact_page_tsx_0q7f9ib._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_ContactForm_tsx_020p9r8._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_page_tsx_0q7f9ib._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_PlanSlider_tsx_01r9.7p._.js"],
-    source: "dynamic"
-});
